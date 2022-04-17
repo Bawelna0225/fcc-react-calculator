a57b6cf6626f0fe6fca8960445f906100cf8fa57
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-04-17 15:30:03 +0200

    calculator css & html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import './styles.css';
+
+
+const App = () => {
+  const [result, setResult] = useState('')
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.innerText))
+  }
+ 
+  const clear = () => {
+    setResult("0")
+  }
+
+  const del = () => {
+    setResult(result.slice(0, result.length - 1))
+  }
+
+  const calculate = () => {
+    setResult(eval(result).toString())
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc-grid">
+      <div id='display'>
+        {result || '0'}
+      </div>
+        <button id='clear' className='span-two' onClick={clear}>AC</button>
+        <button id='delete' onClick={del}>DEL</button>
+        <button id='divide' onClick={handleClick}>/</button>
+        <button id='one' onClick={handleClick}>1</button>
+        <button id='two' onClick={handleClick}>2</button>
+        <button id='three' onClick={handleClick}>3</button>
+        <button id='multiply' onClick={handleClick}>*</button>
+        <button id='four' onClick={handleClick}>4</button>
+        <button id='five' onClick={handleClick}>5</button>
+        <button id='six' onClick={handleClick}>6</button>
+        <button id='add' onClick={handleClick}>+</button>
+        <button id='seven' onClick={handleClick}>7</button>
+        <button id='eight' onClick={handleClick}>8</button>
+        <button id='nine' onClick={handleClick}>9</button>
+        <button id='subtract' onClick={handleClick}>-</button>
+        <button id='decimal' onClick={handleClick}>.</button>
+        <button id='zero' onClick={handleClick}>0</button>
+        <button id='equals' className='span-two' onClick={calculate}>=</button>
     </div>
   );
 }
